9f88ff8b9dbaee09cb13ad4abfaa91c4c34b63b7
GibbZ-78 <95296501+GibbZ-78@users.noreply.github.com>
2022-05-12 21:25:09 +0200

    Backup 21h25

--- src/components/MarkdownInput/MarkdownInput.jsx
+++ src/components/MarkdownInput/MarkdownInput.jsx
@@ -6,26 +6,34 @@ const MarkDownInput = (props) => {
   const [myState, setState] = useState({myTitle: "", myContent: ""});
 
   const handleSubmit = (event) => {
+    
+    event.preventDefault();
+    
     console.log("MarkDownInput says:");
     console.log("  > Trying to add current note to your localStorage");
     
+    let myTmpTitle = (myState.myTitle !== null && myState.myTitle !== undefined) ? myState.myTitle : "<empty title>";
+    let myTmpContent = (myState.myContent !== null && myState.myContent !== undefined) ? myState.myContent : "<empty content>";
+    let myCurrentNote = {title: myTmpTitle, content: myTmpContent};
+
     if (localStorage.getItem('GibbZReactNotepad')) { 
-      console.log("  > localStorage available");
       let myBackedUpNotes = JSON.parse(localStorage.getItem('GibbZReactNotepad'));
-      let myTmpTitle = (myState.myTitle !== null && myState.myTitle !== undefined) ? myState.myTitle : "<empty title>";
-      let myTmpContent = (myState.myContent !== null && myState.myContent !== undefined) ? myState.myContent : "<empty content>";
-      let myCurrentNote = {title: myTmpTitle, content: myTmpContent}
+      console.log("  > localStorage available hence enriching it");
       myBackedUpNotes.push(myCurrentNote);
       console.log("  > Current note added to overall backup var");
       console.log(myBackedUpNotes);
       let myLocalStorage = JSON.stringify(myBackedUpNotes);
-      console.log("  > Backup var stringify to integrate localStorage");
+      console.log("  > Backup var stringified to integrate localStorage");
       console.log(myLocalStorage);
       localStorage.setItem('GibbZReactNotepad', myLocalStorage);
     } else {
-      console.log("  > An error occured when accessing your localStorage. Your browser might not be recent enough or localStorage might be disengaged.");
+      console.log("  > localStorage not initialized yet. Working on it!");
+      let myLocalStorage = JSON.stringify([myCurrentNote]);
+      console.log("  > Backup var stringified as 1st record of new localStorage");
+      console.log(myLocalStorage);
+      localStorage.setItem('GibbZReactNotepad', myLocalStorage);
     }
-    event.preventDefault();
+
   }
 
   const handleChangeTitle = (event) => {
